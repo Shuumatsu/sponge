06511e7aec4457ac0f307efd99ecfc9752d468b2
Akatsuki <VanishingDante@gmail.com>
2021-04-13 19:00:06 -0700

    my implementation of stream_reassembler

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -4,6 +4,9 @@
 #include "byte_stream.hh"
 
 #include <cstdint>
+#include <deque>
+#include <functional>
+#include <iostream>
 #include <string>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
@@ -12,8 +15,27 @@ class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
 
+    bool _eof = false;
+
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    size_t _unassembled_bytes = 0;
+
+    uint64_t _waiting_index = 0;
+
+    struct block {
+        size_t index;
+        string data;
+        struct block &operator=(struct block other) noexcept {
+            this->data = other.data;
+            this->index = other.index;
+            return *this;
+        }
+    };
+
+    deque<struct block> _blocks = {};
+
+    void _insert_block(struct block incomming);
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -46,6 +68,8 @@ class StreamReassembler {
     //! \brief Is the internal state empty (other than the output stream)?
     //! \returns `true` if no substrings are waiting to be assembled
     bool empty() const;
+
+    uint64_t waiting_index() const { return _waiting_index; }
 };
 
 #endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
